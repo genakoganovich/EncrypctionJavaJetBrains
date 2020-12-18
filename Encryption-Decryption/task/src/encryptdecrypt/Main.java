c583e0a74dc9fd576e056814b95184dc68d73718
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-18 17:08:57 +0200

    0002 stage 2/6 done

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,23 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final int SIZE = 26;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] message = scanner.nextLine().toCharArray();
+        int key = scanner.nextInt();
+        for (int i = 0; i < message.length; i++) {
+            if (Character.isAlphabetic(message[i])) {
+                message[i] = (char) ('a' + (message[i] - 'a' + key) % SIZE);
+            }
+        }
+        System.out.println(String.valueOf(message));
+    }
+
+    public static void stageOne() {
         char[] input = "we found a treasure!".toCharArray();
         for (int i = 0; i < input.length; i++) {
             if (Character.isAlphabetic(input[i])) {
